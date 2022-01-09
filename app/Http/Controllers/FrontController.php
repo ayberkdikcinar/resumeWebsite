87d2655f1bdc3ef1a4d31ea4768d27d2953ff617
zaferyalcin <zaferyalcinn@gmail.com>
2022-01-09 14:44:41 +0300

    password change page designed

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,6 +6,8 @@ use App\Models\Homepage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Page;
+use Faker\Provider\ar_EG\Company;
+
 class FrontController extends Controller
 {
     public function login(){
